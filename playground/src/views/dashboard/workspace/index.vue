3f9aca838ec91c669126c9137e7e0e8b038dfb08
bifangknt <1320414964@qq.com>
2025-04-25 17:16:35 +0800

    fix: 使用 $t 函数国际化模块名称

--- playground/src/views/dashboard/workspace/index.vue
+++ playground/src/views/dashboard/workspace/index.vue
@@ -14,6 +14,7 @@ import {
   WorkbenchTodo,
   WorkbenchTrends,
 } from '@vben/common-ui';
+import { $t } from '@vben/locales';
 import { preferences } from '@vben/preferences';
 import { useUserStore } from '@vben/stores';
 import { openWindow } from '@vben/utils';
@@ -181,7 +182,7 @@ const fetchApiLogs = async () => {
 
         return {
           avatar: `svg:avatar-${avatarIndex}`,
-          content: `在 <a>${item.moduleNameCN}</a> 执行了 ${operation} 操作`,
+          content: `在 <a>${$t(item.moduleNameCN)}</a> 执行了 ${operation} 操作`,
           date,
           title: item.username || '匿名用户',
           url: item.refererPath,
