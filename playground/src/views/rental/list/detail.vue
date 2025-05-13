f660ee3de916929c277026b2024aa027b18d4fb0
yuki <844226000@qq.com>
2025-05-13 09:20:06 +0800

    refactor: 优化变压器信息展示布局
    将变压器信息展示从Card组件改为div布局，并调整Descriptions的列配置，提升页面可读性和响应式表现

--- playground/src/views/rental/list/detail.vue
+++ playground/src/views/rental/list/detail.vue
@@ -507,7 +507,6 @@ onMounted(() => {
                         <div
                           v-for="item in factory.firefighting"
                           :key="item.firefightingId"
-                          size="small"
                           class="flex flex-col md:flex-row"
                         >
                           <div class="p-2 md:w-2/3">
@@ -568,43 +567,52 @@ onMounted(() => {
                         "
                         class="space-y-4"
                       >
-                        <Card
+                        <div
                           v-for="item in factory.transformers"
                           :key="item.transformerId"
-                          size="small"
+                          class="flex flex-col md:flex-row"
                         >
-                          <div class="flex flex-col md:flex-row">
-                            <!-- REMOVED: Transformer Image -->
-                            <div class="p-2 md:w-full">
-                              <h3 class="mb-2 text-lg font-bold">
-                                {{ item.title }}
-                              </h3>
-                              <Descriptions :column="1" size="small">
-                                <Descriptions.Item label="地址">
-                                  {{ item.address }}
-                                </Descriptions.Item>
-                                <Descriptions.Item label="联系人">
-                                  {{ item.contact }}
-                                </Descriptions.Item>
-                                <Descriptions.Item label="状态">
+                          <div class="p-2 md:w-2/3">
+                            <h3 class="mb-2 text-lg font-semibold">
+                              {{ item.title }}
+                            </h3>
+                            <Descriptions
+                              bordered
+                              :column="{
+                                xxl: 3,
+                                xl: 2,
+                                lg: 2,
+                                md: 1,
+                                sm: 1,
+                                xs: 1,
+                              }"
+                            >
+                              <Descriptions.Item label="地址">
+                                {{ item.address }}
+                              </Descriptions.Item>
+                              <Descriptions.Item label="联系人">
+                                {{ item.contact }}
+                              </Descriptions.Item>
+                              <Descriptions.Item label="状态">
+                                <Tag :color="getTagColor(item.status)">
                                   {{ item.status }}
-                                </Descriptions.Item>
-                                <Descriptions.Item label="规格">
-                                  {{ item.specifications }}
-                                </Descriptions.Item>
-                                <Descriptions.Item label="检查时间">
-                                  {{ formatDateTime(item.checkTime) }}
-                                </Descriptions.Item>
-                                <Descriptions.Item
-                                  v-if="item.remark"
-                                  label="备注"
-                                >
-                                  {{ item.remark }}
-                                </Descriptions.Item>
-                              </Descriptions>
-                            </div>
+                                </Tag>
+                              </Descriptions.Item>
+                              <Descriptions.Item label="规格">
+                                {{ item.specifications }}
+                              </Descriptions.Item>
+                              <Descriptions.Item label="检查时间">
+                                {{ formatDateTime(item.checkTime) }}
+                              </Descriptions.Item>
+                              <Descriptions.Item
+                                v-if="item.remark"
+                                label="备注"
+                              >
+                                {{ item.remark }}
+                              </Descriptions.Item>
+                            </Descriptions>
                           </div>
-                        </Card>
+                        </div>
                       </div>
                       <div v-else class="py-10 text-center text-gray-500">
                         暂无变压器信息
