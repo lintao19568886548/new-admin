666eac93a243269a3f73565c515d414395ecbb15
jo1o <jojoconnection@163.com>
2025-10-13 10:47:04 +0800

    docs: 更新版权信息中的公司名称和备案链接
    将公司名称简化为备案号，并将备案链接更新为工信部官网

--- playground/src/preferences.ts
+++ playground/src/preferences.ts
@@ -16,8 +16,8 @@ export const overridesPreferences = defineOverridesPreferences({
     showHome: true,
   },
   copyright: {
-    companyName: '宜租网络 粤ICP备2025409677号-1',
-    companySiteLink: 'https://www.yizuw.cn',
+    companyName: '粤ICP备2025409677号-1',
+    companySiteLink: 'https://beian.miit.gov.cn/',
     date: '2025',
     enable: true,
     icp: '',
